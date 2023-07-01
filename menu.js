c47992943a1f896d72d210a222c22a3efe6b18f0
chikchicly <chikchicly@gmail.com>
2023-07-01 09:53:24 +0700

    oK

--- menu.js
+++ menu.js
@@ -1,1 +1,8 @@
-document.createElement('nav');
+const menu=document.createElement('nav');
+let arrayMenu=['/localhost/threePage.html','/localhost/twoPage.html','/localhost/'];
+function eachMenu(x){
+    return '<li><a href='+x[0]+'>'+ x[0] + '</a></li>'+'<li><a href='+x[1]+'>'+ x[1]+ '</a></li>'+'<li><a href='+x[2]+'>'+ x[2]+ '</a></li>';
+}
+menu.innerHTML=eachMenu(arrayMenu);
+menu.style.cssText=`display:fixed;position:absolute;top:15px;right:1%;`;
+document.querySelector('body').append(menu);
